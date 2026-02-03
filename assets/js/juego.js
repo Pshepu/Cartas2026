37fe85997c1ba90b99bfb6527eafe41afc1eab66
Pshepu <paoloasepulveda@gmail.com>
2026-02-03 19:52:42 -0300

    funcion pedir carta practicamente lista, empiezo turno computadora

--- assets/js/juego.js
+++ assets/js/juego.js
@@ -8,6 +8,20 @@ spades
 let deck = []
 const tipos = ['C','D','H','S']
 const especiales = ['A','J','Q','K']
+let puntosJugador = 0,
+    puntosComputadora = 0;
+
+let puntosHTML = document.querySelectorAll('small')
+
+
+//Referencias del HTML
+const btnPedir = document.querySelector('#btnPedir');
+const btnNuevo = document.querySelector('#btnNuevo');
+const btnDetener = document.querySelector('#btnDetener');
+
+const divCartasJugador = document.querySelector('#jugador-cartas')
+
+
 const crearDeck = ()=>{
     for( let i = 2; i<=10; i++){
         for(let tipo of tipos){
@@ -22,11 +36,10 @@ const crearDeck = ()=>{
     
     //para desornedar el mazo se ocupa UNDERSCORE.JS una libreria
     deck = _.shuffle(deck) //ver documentacion para el shuffle
-    console.log(deck)
     return deck
 }
 
-//crearDeck();
+crearDeck();
 
 
 const pedirCarta = ()=>{
@@ -35,8 +48,6 @@ const pedirCarta = ()=>{
         throw 'No hay cartas en el deck'
     }
     const carta = deck.pop()
-    console.log(deck)
-    console.log(carta)
     return carta;
 
 }
@@ -50,5 +61,41 @@ return  (isNaN(valor)) ? ((valor === 'A') ? 11:10): valor*1
 
 }
 
-valor = valorCarta('2H')
-console.log(valor)
+
+//Turno computadora
+
+const turnoComputadora = (puntosMinimos) => {
+
+}
+
+
+
+
+//eventos
+
+btnPedir.addEventListener('click', () =>{
+ const carta = pedirCarta();
+
+ puntosJugador = puntosJugador + valorCarta(carta);
+
+ console.log(puntosJugador)
+
+ puntosHTML[0].innerHTML = puntosJugador
+
+// <img class="carta" src="/assets/cartas/10C.png"> 
+
+const imgCarta = document.createElement('img')
+imgCarta.src = `/assets/cartas/${carta}.png`;
+imgCarta.classList.add('carta')
+divCartasJugador.append(imgCarta)
+
+if(puntosJugador>21){
+    console.warn('Lo siento, Perdiste');
+    btnPedir.disabled = true
+
+}else if( puntosJugador === 21){
+    console.warn('21, Ganaste')
+    btnPedir.disabled = true
+}
+
+})
